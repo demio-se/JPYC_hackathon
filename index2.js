ed60e6e9e1ef79dfa6465624a9ce3ee91c14b41f
demio <ytkmn.blog1@gmail.com>
2022-04-18 06:34:52 +0900

    JPYCへの接続できるか

--- index2.js
+++ index2.js
@@ -23,15 +23,61 @@ async function myFunction2(){
   alert(addresses[0]);
 }
 
+async function myFunctionJPYC(){
+  console.log("JPYC Start");
 
-window.onload = async function(){
-  myFunction();
-  myFunction2();
+  const provider = await new ethers.providers.Web3Provider(window.ethereum);
+  const addresses = await etherum.request({method: 'eth_requestAccounts'});
+  
+  // The MetaMask plugin also allows signing transactions to
+  // send ether and pay to change state within the blockchain.
+  // For this, you need the account signer...
+  const signer = await provider.getSigner();
+ 
+  // You can also use an ENS name for the contract address
+  //JPYC Test Net address
+  const JPYCAddress = "0xbD9c419003A36F187DAf1273FCe184e1341362C0";
+
+  // The ERC-20 Contract ABI, which is a common contract interface
+  // for tokens (this is the Human-Readable ABI format)
+  const JPYCAbi = [
+    // Some details about the token
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+
+    // Get the account balance
+    "function balanceOf(address) view returns (uint)",
+
+    // Send some of your tokens to someone else
+    "function transfer(address to, uint amount)",
+
+    // An event triggered whenever anyone transfers to someone else
+    "event Transfer(address indexed from, address indexed to, uint amount)"
+  ];
+
+  // The Contract object
+  const JPYCContract = await new ethers.Contract(JPYCAddress, JPYCAbi, provider);
+  
+  await JPYCContract.name();
+  console.log(JPYCContract.name());
+
+  await JPYCContract.symbol();
+  console.log(JPYCContract.symbol());
+
+  const balance = await JPYCContract.balanceOf(addresses[0]);
+  console.log(JPYCContract.balanceOf(addresses[0]));
+
+
+  console.log("JPYC End");
+  
 }
 
-const ethereumButton = document.querySelector('.enableEthereumButton');
+window.onload = async function(){
+  //myFunction();
+  //myFunction2();
+  myFunctionJPYC();
+}
 
-ethereumButton.addEventListener('click', () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: 'eth_requestAccounts' });
-});
+
+
+
