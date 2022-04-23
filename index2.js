a433d6583fc0ea9384baccc2e77d5a8d0dd216f0
demio <ytkmn.blog1@gmail.com>
2022-04-24 01:11:35 +0900

    時間変更

--- index2.js
+++ index2.js
@@ -4,7 +4,7 @@ const testSmartContract = "0x8952A41f42560DD04230aF292D7543B6dDB98A48"; //Rinkeb
 //JPYCのコントラクトアドレス。テストネット。Rinkebey
 //JPYC Test Net address
 const JPYCAddress = "0xbD9c419003A36F187DAf1273FCe184e1341362C0";
-let time ="22/04/24 0104";
+let time ="22/04/24 0111";
 
 let provider;
 let providerSC;
